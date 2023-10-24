e16811718eb0ac9a9c233f30c4dcb503921ff57c
yovany samaca <egsamca56@gmail.com>
2023-10-23 22:22:53 -0500

    Algoritmos con el score mas alto

--- icfes_clasifier.py
+++ icfes_clasifier.py
@@ -2,27 +2,14 @@ import pandas as pd
 import sklearn
 import matplotlib.pyplot as plt
 
-from sklearn.decomposition import PCA
-from sklearn.decomposition import IncrementalPCA
-    
-from sklearn.linear_model import LogisticRegression 
-
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 
-from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import BaggingClassifier
-from sklearn.ensemble import GradientBoostingClassifier
+
 
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
-from sklearn.svm import LinearSVC
-from sklearn.linear_model import SGDClassifier
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.ensemble import BaggingClassifier
-
-
 
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -33,7 +20,7 @@ from sklearn.metrics import accuracy_score
 import warnings
 warnings.simplefilter('ignore')
 
-icfes = pd.read_csv('/Users/giovanysamaca/Desktop/git proyecto icfes/data.csv')
+icfes = pd.read_csv('/Users/giovanysamaca/Desktop/icfes_git/data.csv')
 
 icfes = icfes.sample(3000)
 
@@ -52,23 +39,15 @@ preprocessed_data = StandardScaler().fit_transform(raw_data)
 
 X_train, X_test, y_train, y_test = train_test_split(raw_data, target, test_size=0.3, random_state=0)
 
-estimators = {
-       'LogisticRegression' : LogisticRegression(),
-        'SVC' : SVC(),
-        'LinearSVC' : LinearSVC(),
-        'SGD' : SGDClassifier(loss="hinge", penalty="l2", max_iter=5),
-        'KNN' : KNeighborsClassifier(),
-        'DecisionTreeClf' : DecisionTreeClassifier(),
-        'RandomTreeForest' : RandomForestClassifier(random_state=0),
-        'Boosting' : GradientBoostingClassifier(n_estimators=50)
-        
-    }
 
+model = SVC()
+# Aplico bagging al algoritmo de clasificacion
+bag_class = BaggingClassifier(base_estimator=model, n_estimators=50).fit(X_train, y_train)
 
-for name, estimator in estimators.items():
-    bag_class = BaggingClassifier(base_estimator=estimator, n_estimators=50).fit(X_train, y_train)
-    bag_predict = bag_class.predict(X_test)
-    print('='*64)
-    print('SCORE Bagging with {} : {}'.format(name, accuracy_score(bag_predict, y_test)))
+# Predicciones
 
+predictions = bag_class.predict(X_test)
+
+# Resultado
+print('SCORE Bagging with Support Vector Classification {}'.format(accuracy_score(predictions, y_test)))
 
--- icfes_regresion.py
+++ icfes_regresion.py
@@ -1,9 +1,6 @@
 import pandas as pd
 
-from sklearn.linear_model import (
-    RANSACRegressor, HuberRegressor, Lasso, Ridge
-)
-from sklearn.svm import SVR
+from sklearn.linear_model import Lasso
 
 from sklearn.model_selection import train_test_split
 from sklearn.metrics  import mean_squared_error
@@ -14,7 +11,7 @@ import seaborn as sns
 import warnings
 warnings.simplefilter("ignore")
 
-icfes = pd.read_csv('/Users/giovanysamaca/Desktop/git proyecto icfes/data.csv')
+icfes = pd.read_csv('/Users/giovanysamaca/Desktop/icfes_git/data.csv')
 
 icfes = icfes.sample(3000)
 
@@ -30,18 +27,7 @@ y = icfes[['PUNT_GLOBAL']]
 
 X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.3, random_state=42)
 
-estimadores = {
-        'SVR' : SVR(gamma = 'auto', C=1.0, epsilon=0.1),
-        'RANSAC' : RANSACRegressor(),
-        'HUBER' : HuberRegressor(epsilon=1.35),
-        'LASSO' : Lasso(alpha=0.02),
-        'RIDGE' : Ridge(alpha=1)
-    }
+model = Lasso(alpha=0.02)
+model.fit(X_train,y_train)
 
-for name, estimator in estimadores.items():
-        estimator.fit(X_train, y_train)
-        predictions = estimator.predict(X_test)
-        print("="*64)
-        print(name)
-        print("MSE: ", mean_squared_error(y_test, predictions))
-        print("Score", estimator.score(X_test, y_test))
+print("Score Lasso", model.score(X_test, y_test))
